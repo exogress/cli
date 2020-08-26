0f9508f8338fa7e181eeef2cb1ededadf339db65
Gleb Pomykalov <glebpom@gmail.com>
2020-08-26 19:39:45 +0300

    Support init command (#4)
    Support init command

---
+++ src/init.rs
@@ -0,0 +1,22 @@
+use exogress_config_core::{Config, DEFAULT_CONFIG_FILE};
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::path::Path;
+
+pub fn create_exofile(dir: impl AsRef<Path>) -> Result<(), anyhow::Error> {
+    let file_path = dir.as_ref().join(DEFAULT_CONFIG_FILE);
+
+    let cfg = Config::sample(None, None, None, None);
+
+    let str = serde_yaml::to_vec(&cfg)?;
+
+    let mut f = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(file_path)?;
+    f.write_all(str.as_ref())?;
+
+    info!("{} successfully created", DEFAULT_CONFIG_FILE);
+
+    Ok(())
+}
