a0ad4ec20d13ff67f3f1f88c57f9b409c23c9545
Gleb Pomykalov <gleb@exogress.com>
2020-12-15 22:54:47 +0300

    upgrade deps

--- src/init.rs
+++ src/init.rs
@@ -1,4 +1,4 @@
-use exogress_config_core::{ClientConfig, DEFAULT_CONFIG_FILE};
+use exogress::config_core::{ClientConfig, DEFAULT_CONFIG_FILE};
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::Path;
